56cf76332c15bee3835f606a41e1925724a14cb5
Josephadelin22 <j.boussamba@alustudent.com>
2026-05-01 16:54:14 +0200

    New changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
   <TooltipProvider>
     <Toaster />
     <Sonner />
-    <BrowserRouter>
+    <BrowserRouter basename="/embassy-pass/">
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/auth" element={<Auth />} />
